313180c0c128e77db4370808711413526905b209
sjfu@nctu.edu.tw <sjfu@nctu.edu.tw>
2017-04-25 20:14:50 +0800

    Add triangle C0, C1, C2

--- triangle/test.cpp
+++ triangle/test.cpp
@@ -8,62 +8,32 @@ Robust values {0,201}
 #include "gtest/gtest.h"
 #include "triangle.hpp"
 
-
-TEST( TriangleTest, NormalBoundary ) {
-	//one at min bound
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(MIN, 100, 100) );
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(100, MIN, 100) );
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(100, 100, MIN) );
-	//one at min+1 bound
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(MIN+1, 100, 100) );
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(100, MIN+1, 100) );
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(100, 100, MIN+1) );
-	//all same
-	ASSERT_EQ( TYPE_EQUILATERAL, triangle_type(100, 100, 100) );
-	//one at max-1 bound
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(MAX-1, 100, 100) );
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(100, MAX-1, 100) );
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(100, 100, MAX-1) );
-	//one at max bound
-	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(MAX, 100, 100) );
-	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(100, MAX, 100) );
-	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(100, 100, MAX) );
-	//ref: http://www.softwaretestinggenius.com/tutorial-2-to-generate-bva-test-cases-for-the-triangle-problem
-}
-
-TEST( TriangleTest, Equivalence ) {
-	//  Weak Normal Equivalence Class
+// Statement coverage
+TEST( TriangleTest, CodeCoverageC0 ) {
+	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(1, 2, 3) );
 	ASSERT_EQ( TYPE_EQUILATERAL, triangle_type(5, 5, 5) );
 	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(2, 2, 3) );
 	ASSERT_EQ( TYPE_SCALENE, triangle_type(3, 4, 5) );
-	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(4, 1, 2) );
-	// Weak Robust Equivalence Class 
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(-1, 5, 5) ); //strong Robust single invaled
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(5, -1, 5) );//strong Robust single invaled
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(5, 5, -1) );//strong Robust single invaled
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(201, 5, 5) );
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(5, 201, 5) );
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(5, 5, 201) );
-	// Strong Robust Equivalence Class two invaled
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(-1, -1, 5) );
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(5, -1, -1) );
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(-1, 5, -1) );
-	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(-1, -1, -1) );
-	//ref: http://www.softwaretestinggenius.com/tutorial-6-to-generate-equivalence-class-test-cases-for-the-triangle-problem
 }
 
-TEST( TriangleTest, DecisionTable ) {
-	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(4, 1, 2) );
-	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(1, 4, 2) );
-	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(1, 2, 4) );
+// DD-path coverage
+TEST( TriangleTest, CodeCoverageC1 ) {
+	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(-1, 1, 1) );
+	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(1, 2, 3) );
 	ASSERT_EQ( TYPE_EQUILATERAL, triangle_type(5, 5, 5) );
 	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(2, 2, 3) );
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(3, 2, 2) );
-	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(2, 3, 2) );
 	ASSERT_EQ( TYPE_SCALENE, triangle_type(3, 4, 5) );
-
 }
 
+// DD-path coverage + Simple loop coverage
+TEST( TriangleTest, CodeCoverageC2 ) {
+	//same as CodeCoverageC1 because of Triangle Problem doesn't have a Loop
+	ASSERT_EQ( TYPE_INVALID_INPUT, triangle_type(-1, 1, 1) );
+	ASSERT_EQ( TYPE_NOTTRIANGLE, triangle_type(1, 2, 3) );
+	ASSERT_EQ( TYPE_EQUILATERAL, triangle_type(5, 5, 5) );
+	ASSERT_EQ( TYPE_ISOSCELES, triangle_type(2, 2, 3) );
+	ASSERT_EQ( TYPE_SCALENE, triangle_type(3, 4, 5) );
+}
 
 int main( int argc, char *argv[] ) {
 	::testing::InitGoogleTest(&argc, argv);
