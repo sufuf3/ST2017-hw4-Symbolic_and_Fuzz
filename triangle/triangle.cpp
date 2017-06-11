7fbaa7eb5acccee8b2d02180b225556fde0d7930
Samina Fu <sufuf3@gmail.com>
2017-06-11 15:56:23 +0000

    Make klee symbolic of triangle

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "triangle.hpp"
+#include "klee/klee.h"
 
 int triangle_type( int a, int b, int c ) {
 	if (a < 1 || b < 1 || c < 1 || a > 200 || b > 200 || c > 200)
@@ -13,4 +14,12 @@ int triangle_type( int a, int b, int c ) {
 			return TYPE_ISOSCELES;
 	}
 	return TYPE_SCALENE;
+}
+
+int main() {
+	int a, b, c;
+	klee_make_symbolic(&a, sizeof(a), "a");
+	klee_make_symbolic(&b, sizeof(b), "b");
+	klee_make_symbolic(&c, sizeof(c), "c");
+	return triangle_type(a, b, c);
 }
