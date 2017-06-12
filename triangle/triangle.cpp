89960fee945ea8245803fb876921adb27a4cbdf4
Samina <sufuf3@gmail.com>
2017-06-12 11:17:42 +0800

    Fix triangle.cpp to fit the fuzz format

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "triangle.hpp"
-#include "klee/klee.h"
+//#include "klee/klee.h"
 
 int triangle_type( int a, int b, int c ) {
 	if (a < 1 || b < 1 || c < 1 || a > 200 || b > 200 || c > 200)
@@ -18,8 +18,8 @@ int triangle_type( int a, int b, int c ) {
 
 int main() {
 	int a, b, c;
-	klee_make_symbolic(&a, sizeof(a), "a");
-	klee_make_symbolic(&b, sizeof(b), "b");
-	klee_make_symbolic(&c, sizeof(c), "c");
+	//klee_make_symbolic(&a, sizeof(a), "a");
+	//klee_make_symbolic(&b, sizeof(b), "b");
+	//klee_make_symbolic(&c, sizeof(c), "c");
 	return triangle_type(a, b, c);
 }
